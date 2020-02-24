355adc7a16a348180829d88f6398bcfac0e0f478
anthony amaro <anthonyamaro@anthonys-MacBook-Pro.local>
2020-02-24 11:32:13 -0800

    getCarInfoById() done

--- index.js
+++ index.js
@@ -183,8 +183,13 @@ function getLastCarInfo(arr) {
  * For example, if getCarInfoById is invoked with the inventory and the number 1,
  * it will return `This is a Lincoln Navigator`.
  */
-function getCarInfoById(/* code here */) {
+function getCarInfoById(array, id) {
   /* code here */
+  for (let i = 0; i < array.length; i++) {
+    if (array[i].id === id) {
+      return `This is a ${array[i].car_make} ${array[i].car_model}`;
+    }
+  }
 }
 
 /**
