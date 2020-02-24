0f03bfe469e96a2f4856fe9a84247d2cf8fe4841
anthony amaro <anthonyamaro@anthonys-MacBook-Pro.local>
2020-02-24 08:49:53 -0800

    makeSmartPerson() done

--- index.js
+++ index.js
@@ -80,8 +80,18 @@ function getName(obj) {
  *         and returns a string like `Hello, my name is {name}`.
  *         where `{name}` is the name passed into `makeSmartPerson`.
  */
-function makeSmartPerson(/* code here */) {
+function makeSmartPerson(name) {
   /* code here */
+  const obj = {
+    name,
+    sum: function(num1, num2) {
+      return num1 + num2;
+    },
+    speak: function() {
+      return `Hello, my name is ${this.name}`;
+    }
+  };
+  return obj;
 }
 
 /*
