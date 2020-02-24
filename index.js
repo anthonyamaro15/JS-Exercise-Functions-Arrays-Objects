6478d179d193a310c18adc21f00ebff549611b2b
anthony amaro <anthonyamaro@anthonys-MacBook-Pro.local>
2020-02-24 11:56:51 -0800

    getOlderCars() done

--- index.js
+++ index.js
@@ -253,9 +253,33 @@ function getModelYears(array) {
  * with a `car_year` which is at most the given desired max year,
  * in the same order as they appear in the original inventory.
  */
-function getOlderCars(/* code here */) {
+function getOlderCars(array, year) {
   /* code here */
+  let result = [];
+  for (let i = 0; i < array.length; i++) {
+    if (array[i].car_year <= year) {
+      result.push(array[i]);
+    }
+  }
+  return result;
 }
+
+// console.log(
+//   getOlderCars(
+//     [
+//       { id: 1, car_make: "Lincoln", car_model: "Navigator", car_year: 2009 },
+//       { id: 2, car_make: "Mazda", car_model: "Miata MX-5", car_year: 2001 },
+//       {
+//         id: 3,
+//         car_make: "Land Rover",
+//         car_model: "Defender Ice Edition",
+//         car_year: 2010
+//       },
+//       { id: 4, car_make: "Honda", car_model: "Accord", car_year: 1983 }
+//     ],
+//     2000
+//   )
+// );
 
 /**
  * ### Challenge `getGermanCars`
